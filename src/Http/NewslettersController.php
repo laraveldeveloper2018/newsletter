305542e0e786ac313e3a18bf54c6facc3640cc4d
alicompu <2014najafi@gmail.com>
2019-08-01 01:30:32 +0430

    modify the controller class

--- src/Http/NewslettersController.php
+++ src/Http/NewslettersController.php
@@ -1,12 +1,2 @@
-{
-    "name": "najafi/newsletter",
-    "description": "This package for used newsletters",
-    "license": "MIT",
-    "authors": [
-        {
-            "name": "Ali Najafi",
-            "email": "2014najafi@gmail.com"
-        }
-    ],
-    "require": {}
-}
+<?php
+echo "Hello world";
